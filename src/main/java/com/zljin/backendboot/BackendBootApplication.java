1daa023daaf4039a23728bee74a69adf4e429c44
leonard <leonard@qq.com>
2025-01-05 18:44:11 +0800

    init gradle

---
+++ src/main/java/com/zljin/backendboot/BackendBootApplication.java
@@ -0,0 +1,13 @@
+package com.zljin.backendboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendBootApplication.class, args);
+    }
+
+}
